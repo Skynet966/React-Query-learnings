ecc1664b4ad98991dcf42dcfc551283addce5095
Satish Yadav <satish.yadav@nagarro.com>
2022-09-28 16:06:52 +0530

    React query dev tool added

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,6 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
+			<ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
 		</QueryClientProvider>
 	</React.StrictMode>,
 );
