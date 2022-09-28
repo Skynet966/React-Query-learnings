ce7a5c429d82131d2e39ebb91662f2e5a41bc0e2
Satish Yadav <satish.yadav@nagarro.com>
2022-09-28 14:39:28 +0530

    First react-query offical example

--- src/components/Home.page.jsx
+++ src/components/Home.page.jsx
@@ -1,7 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 
 function HomePage() {
-	return <div>HomePage</div>;
+	const { isLoading, data, error } = useQuery(['repoData'], () =>
+		axios
+			.get('https://api.github.com/repos/Skynet966/React-Query-learnings')
+			.then(res => res.data),
+	);
+
+	if (isLoading) return 'Loading...';
+
+	if (error) return 'An error has occurred: ' + error.message;
+
+	return (
+		<div>
+			<h1>{data.name}</h1>
+			<p>{data.description}</p>
+			<strong>👀 {data.subscribers_count}</strong>{' '}
+			<strong>✨ {data.stargazers_count}</strong>{' '}
+			<strong>🍴 {data.forks_count}</strong>
+		</div>
+	);
 }
 
 export default HomePage;
